89429fb6d148a13ef3ce47f546a82874f52b75a5
Hester Knight <hesterknight@hesters-mbp.mynet>
2019-12-17 15:30:23 +0000

    finished main objectives and one stretch

--- index.js
+++ index.js
@@ -39,9 +39,31 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function (food) {
+  if(food < 11) {
+    this.stomach.push(food)
+  }
+
+  else {
+    return [];
+  }
+
+};
+
+Person.prototype.poop = function() {
+return this.stomach = [];
+}
+
+Person.prototype.toString = function() {
+  return (`${this.name} , ${this.age}`);
+}
+
 
 /*
   TASK 2
@@ -57,7 +79,27 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0; 
+}
+
+
+Car.prototype.fill = function(gallons) {
+  
+  return this.tank + gallons;
+}
+
+Car.prototype.drive = function(distance) {
+  if(distance > 0) {
+    // let count = 0
+    //  this.odometer = count++;
+      return(`I ran out of fuel at ${this.odometer}!`);
+
+
+  }
 
 }
 
@@ -68,15 +110,24 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age, favoriteToy);
+  this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function() {
+  return (`Playing with ${this.favoriteToy}`);
+}
+
+
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
+  1. The 'Global' binding, where the 'this' keyword in the global scope refers to the window or the console object. 
   2. 
   3. 
   4. 
